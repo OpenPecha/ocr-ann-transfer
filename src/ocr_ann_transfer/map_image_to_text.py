fc7afe1fb36b1a42dd757654e2e43a1c89e4f7f4
Tenzin <tenzintsunduebhattu@gmail.com>
2024-04-09 15:53:56 +0530

    map page texts to images

--- src/ocr_ann_transfer/map_image_to_text.py
+++ src/ocr_ann_transfer/map_image_to_text.py
@@ -1,5 +1,6 @@
+import csv 
 from pathlib import Path 
-
+from typing import List 
 
 def create_page_texts(text:str, volume:str, output_dir:Path):
     output_dir.mkdir(parents=True, exist_ok=True)
@@ -9,9 +10,32 @@ def create_page_texts(text:str, volume:str, output_dir:Path):
         Path(output_dir/file_name).write_text(page, encoding="utf-8")
 
 
-if __name__ == "__main__":
-    file_path = Path("new_v003.txt")
-    text = file_path.read_text(encoding="utf-8").replace("\n\n", "$")
-    output_dir = Path("page_texts")
+def sort_paths_by_string(paths:List[Path]) -> List[str]:
+    """ sort paths by string and return as string"""
+    return [str(path) for path in sorted(paths, key=lambda path: str(path))]
 
-    create_page_texts(text, "3", output_dir)
+def map_page_texts_to_images(images_dir:Path, page_texts_dir:Path, output_file_path:Path=Path("page_texts_to_images.csv")):
+    images = list(images_dir.rglob("*.jpg"))
+    page_texts = list(page_texts_dir.rglob("*.txt"))
+
+    if len(page_texts) != len(images):
+        print("[ERROR]: Number of page texts and images are not equal!")
+        return 
+    
+    images = sort_paths_by_string(images)
+    page_texts = sort_paths_by_string(page_texts)
+
+    """ write to csv """
+    with open(output_file_path, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+
+        csvwriter.writerow(['Image Paths', 'Page Text Paths'])
+        for img_path, text_path in zip(images, page_texts):
+            csvwriter.writerow([img_path, text_path])
+
+
+if __name__ == "__main__":
+    images_dir = Path("images/W2PD17382-I1KG81275")
+    page_texts_dir = Path("page_texts")
+
+    map_page_texts_to_images(images_dir, page_texts_dir)
