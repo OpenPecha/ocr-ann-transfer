1059e66ed2f47282d329c874c024f92be701e28b
Tenzin <tenzintsunduebhattu@gmail.com>
2024-04-22 11:15:09 +0530

    standardize_line_texts_to_images_csv_mapping

--- src/ocr_ann_transfer/utility.py
+++ src/ocr_ann_transfer/utility.py
@@ -1,4 +1,6 @@
 import numpy as np 
+import csv 
+import pandas as pd
 from typing import List 
 from pathlib import Path 
 from PIL import Image
@@ -17,3 +19,38 @@ def get_image_as_array(image_path:Path) -> np.array:
     image_array = np.array(image)
     
     return image_array
+
+
+def get_image_url(image_name, batch_id):
+    """ returns amazon s3 buckets link of the uploaded image"""
+    image_url = f"https://s3.amazonaws.com/monlam.ai.ocr/line_to_text/{batch_id}/{image_name}"
+    return image_url
+
+def add_row_to_csv(row, csv_path):
+    if Path(csv_path).exists():
+        with open(csv_path, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(row)
+    else:
+        with open(csv_path, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(row)
+
+
+def standardize_line_texts_to_images_csv_mapping(csv_file_path:Path, batch_id:str, group_id:int, state:str="transcribing"):
+    """ parse the line texts to images mapping """
+    df = pd.read_csv(csv_file_path)
+    images_path = list(df["Image Paths"])
+    line_texts_path = list(df["Line Text Paths"])
+
+    """ standardize """
+    output_file_path = f"{batch_id}.csv"
+    headers = ["id","group_id","batch_id","state","inference_transcript","url"]
+    add_row_to_csv(headers, output_file_path)
+    for image_path, line_texts_path in zip(images_path, line_texts_path):
+        id = Path(image_path).name
+        inference_transcript = Path(line_texts_path).read_text(encoding="utf-8")
+        image_url = get_image_url(id, f"batch_id")
+        row = [id, group_id, batch_id, state, inference_transcript, image_url]
+        add_row_to_csv(row, output_file_path)
+        row = []
